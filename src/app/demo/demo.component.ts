f2ac74c810f843891a286b5dddb58cfb46c74e22
DylanBonga <d_bonga@hotmail.com>
2020-01-02 21:55:56 +0100

    demo shenanegance

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { Event } from '../event/event';
+import { EventService } from './demo.service';
 
 @Component({
   selector: 'demo',
   styleUrls: ['./demo.component.css'],
-  templateUrl: './demo.component.html'
+  templateUrl: './demo.component.html',
+  providers: [EventService]
 })
-export class DemoComponent {}
+
+export class DemoComponent {
+  eventList: Event[] = []
+
+  constructor(private eventService: EventService){
+    this.eventService.getEventList().subscribe(ab => this.eventList = ab);
+  }
+
+
+}
